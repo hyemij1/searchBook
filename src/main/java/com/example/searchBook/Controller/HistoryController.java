ab521f803345e0ee606f147c45a9410d1c109c1f
HYEMI <hamjay11@gmail.com>
2022-04-05 23:09:51 +0900

    history

--- src/main/java/com/example/searchBook/Controller/HistoryController.java
+++ src/main/java/com/example/searchBook/Controller/HistoryController.java
@@ -1,5 +1,6 @@
 package com.example.searchBook.Controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.searchBook.User.model.User;
+import com.example.searchBook.User.service.UserService;
 import com.example.searchBook.book.model.History;
 import com.example.searchBook.book.service.HistoryService;
 
@@ -17,10 +20,22 @@ public class HistoryController {
 	@Autowired
 	private HistoryService historyService;
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping(value = "/history")
 	@ResponseBody
-	public List<History> history(Model model) {
-		return historyService.findByUserId(3L);
+	public List<History> history(Principal principal, Model model) {
+		
+		User loginuser = userService.findByUsername(principal.getName());
+
+		return historyService.findByUserId(loginuser.getId());
+	}
+	
+	@GetMapping(value = "/recent")
+	@ResponseBody
+	public List<History> recent(Principal principal, Model model) {
+		return null;
 	}
 	
 }
